dcf623609f6305824e2d728bdfcb2ac242fed7e2
fedecanesa <federicocanesa@gmail.com>
2024-01-25 11:00:06 -0300

    feat(utils): creates an element enum.

---
+++ src/utils/constants/elementsEmun.ts
@@ -0,0 +1,74 @@
+export const elementsEnum = [
+    {
+        value: "bug",
+        name: "Bug" 
+    },
+    {
+        value: "dark",
+        name: "Dark" 
+    },
+    {
+        value: "dragon",
+        name: "Dragon" 
+    },
+    {
+        value: "electric",
+        name: "Electric" 
+    },
+    {
+        value: "fairy",
+        name: "Fairy" 
+    },
+    {
+        value: "fighting",
+        name: "Fighting" 
+    },
+    {
+        value: "fire",
+        name: "Fire" 
+    },
+    {
+        value: "flying",
+        name: "Flying" 
+    },
+    {
+        value: "ghost",
+        name: "Ghost" 
+    },
+    {
+        value: "grass",
+        name: "Grass" 
+    },
+    {
+        value: "ground",
+        name: "Ground" 
+    },
+    {
+        value: "ice",
+        name: "Ice" 
+    },
+    {
+        value: "normal",
+        name: "Normal" 
+    },
+    {
+        value: "poison",
+        name: "Poison" 
+    },
+    {
+        value: "psychic",
+        name: "Psychic" 
+    },
+    {
+        value: "rock",
+        name: "Rock" 
+    },
+    {
+        value: "steel",
+        name: "Steel" 
+    },
+    {
+        value: "water",
+        name: "Water" 
+    },
+];
---
+++ src/utils/constants/index.ts
@@ -0,0 +1,4 @@
+import { elementsEnum } from "./elementsEmun";
+import { routesEnum } from "./routesEnum";
+
+export {elementsEnum, routesEnum}
