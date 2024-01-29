69d21606247b1a85b291a3a625d92c75d79f68f7
fedecanesa <federicocanesa@gmail.com>
2024-01-29 10:07:47 -0300

    feat(filter): creates the filter component to filter a pokemon and the styles

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,7 +1,35 @@
-const Filter = () => {
-  return (
-    <div>Filter</div>
-  )
+import { elementsEnum } from '../../utils/constants';
+import './filter.scss';
+
+interface FilterProps {
+    selectedElement: string;
+    handleElementClick: (element: string) => void;
 }
 
-export default Filter
+const Filter: React.FC<FilterProps> = ({
+    selectedElement,
+    handleElementClick,
+}) => {
+    return (
+        <section className='filter-container'>
+            <menu className='filter'>
+                {elementsEnum.map(element => {
+                    return (
+                        <button
+                            key={element.value}
+                            className={`filter__button ${
+                                selectedElement === element.value
+                                    ? 'selected'
+                                    : ''
+                            }`}
+                            onClick={() => handleElementClick(element.value)}>
+                            {element.name}
+                        </button>
+                    );
+                })}
+            </menu>
+        </section>
+    );
+};
+
+export default Filter;
